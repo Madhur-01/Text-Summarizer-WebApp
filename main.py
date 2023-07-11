9526aaf580852aa185e74344ee59ab5f873f59eb
Madhur-01 <madhurgarg9926@gmail.com>
2023-07-11 20:07:27 +0530

    done

--- main.py
+++ main.py
@@ -21,10 +21,14 @@ model = AutoModelForSeq2SeqLM.from_pretrained("Madhur-01/text-summarization-mode
 pipe = pipeline('summarization', model = mode,tokenizer=tokenizer )
 
 text_input = st.text_area("Input your text in English")
+# Set max and min length inputs
+max_length = st.number_input("Max Summary Length", min_value=10, max_value=500, value=150)
+min_length = st.number_input("Min Summary Length", min_value=10, max_value=500, value=40)
 
-
+# Summarize the input text on button click
 if st.button("Summarize"):
     if text_input:
-        pred = pipe(text_input, max_length=150, min_length=40, do_sample=False)
+        # Generate summary using the pipeline with custom length settings
+        summaries = pipe(text_input, max_length=max_length, min_length=min_length, do_sample=False)
         st.write("Summarized Text")
         st.write(pred[0].get("summary_text"))
