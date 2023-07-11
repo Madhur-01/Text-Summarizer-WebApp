dd65da24b52c25afe15ce05a9c6fb1d620eb1799
Madhur-01 <madhurgarg9926@gmail.com>
2023-07-11 23:23:29 +0530

    done

--- main.py
+++ main.py
@@ -12,23 +12,20 @@ import torch
 
 st.write("Text Summarizer WebApp")
 
- 
 
-tokenizer = AutoTokenizer.from_pretrained("Madhur-01/text-summarization-model")
+model_ckpt = "Madhur-01/text-summarization-model"
 
-model = AutoModelForSeq2SeqLM.from_pretrained("Madhur-01/text-summarization-model")
+tokenizer = AutoTokenizer.from_pretrained(model_ckpt)
 
-pipe = pipeline('summarization', model = mode,tokenizer=tokenizer )
+model_pegasus = AutoModelForSeq2SeqLM.from_pretrained(model_ckpt)
+
+pipe = pipeline('summarization', model = model_ckpt )
 
 text_input = st.text_area("Input your text in English")
-# Set max and min length inputs
-max_length = st.number_input("Max Summary Length", min_value=10, max_value=500, value=150)
-min_length = st.number_input("Min Summary Length", min_value=10, max_value=500, value=40)
 
-# Summarize the input text on button click
+
 if st.button("Summarize"):
     if text_input:
-        # Generate summary using the pipeline with custom length settings
-        summaries = pipe(text_input, max_length=max_length, min_length=min_length, do_sample=False)
+        pred = pipe(text_input)
         st.write("Summarized Text")
         st.write(pred[0].get("summary_text"))
